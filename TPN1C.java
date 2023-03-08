18a07f866e88e68ec0f176a440861034f398f5c3
SaaDebora <127162776+SaaDebora@users.noreply.github.com>
2023-03-07 22:12:58 -0300

    TPN1CyD-AP

---
+++ TPN1C.java
@@ -0,0 +1,18 @@
+package EjercicioC;
+
+public class TPN1C {
+
+	public static void main(String[] args) {
+		int numeroInicio =5;
+		int numeroFin =14; 
+		//EJERCICION1B
+	       numeroInicio =5;
+		while(numeroInicio <= numeroFin) {
+			if(numeroInicio %2 == 0) {
+				System.out.println(numeroInicio);
+			}
+		numeroInicio++;
+		}
+	}
+
+}
---
+++ ejercicioD.java
@@ -0,0 +1,18 @@
+package EjercicioD;
+
+public class ejercicioD {
+
+	public static void main(String[] args) {
+		// Ejercicio D
+		
+      int numeroInicio = 5;
+		
+		for(int numeroFinal = 14; numeroFinal >= numeroInicio ; numeroFinal--) {
+			if(numeroFinal %2 == 0) {
+				System.out.print("\t" + numeroFinal);
+			}
+			
+		}
+	}
+
+}
